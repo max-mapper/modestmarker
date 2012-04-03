590fae16ee680d77a11379b66a4b60811d66c86f
Max Ogden <mogden@gmail.com>
2012-04-03 15:13:29 -0700

    popup works on touch and mouse

--- modestmarker.js
+++ modestmarker.js
@@ -28,19 +28,37 @@ ModestMarker.prototype.updatePosition = function() {
   var point = me.map.locationPoint(me.location)  
   me.div.style.left = point.x - me.img.width / 2 + 'px'
   me.div.style.top = point.y - me.img.height + 'px'
+  if (me.popup && me.popup.style.display === "block") me.showPopup()
 }
 
-ModestMarker.prototype.setPopup = function(html, offsetY) {
+ModestMarker.prototype.setPopup = function(html, options) {
   var me = this
-  me.offsetY = offsetY || 0
+  me.offsetY = options.offsetY || 0
   me.popup = document.createElement('div')
   me.popup.className = 'popup'
   me.popup.style.display = "none"
   me.popup.style.position = "absolute"
   me.popup.innerHTML = html
   me.map.parent.appendChild(me.popup)
-  me.div.onmouseover = function() { me.showPopup.call(me) }
-  me.div.onmouseout = function() { me.hidePopup.call(me) }
+  
+  function togglePopup() {
+    if (me.popup.style.display === "none") me.showPopup.call(me)
+    else me.hidePopup.call(me)
+  }
+  
+  function dismissPopup(e) { if (e.target !== me.img) me.hidePopup.call(me) }
+  
+  var touch = ('ontouchstart' in window) ? true : false
+  if (touch) me.div.ontouchstart = togglePopup
+  if (touch) me.map.parent.ontouchstart = dismissPopup
+  
+  var mouseHover = (options.hover && !touch)
+  if (mouseHover) me.div.onmouseover = function() { me.showPopup.call(me) }
+  if (mouseHover) me.div.onmouseout = function() { me.hidePopup.call(me) }
+  
+  var mouseClick = (!options.hover && !touch)
+  if (mouseClick) me.map.parent.onmousedown = dismissPopup
+  if (mouseClick) me.div.onmousedown = togglePopup
 }
 
 ModestMarker.prototype.hidePopup = function(e) {
