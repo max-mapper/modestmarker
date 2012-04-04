06b012ee22aa9fce9acf2f1d901809d6dfef0d76
Max Ogden <mogden@gmail.com>
2012-04-04 12:10:56 -0700

    experiment with zepto dependency

--- modestmarker.js
+++ modestmarker.js
@@ -1,76 +1,103 @@
+/*
+ * modestmarker.js - simple markers and popups for modestmaps
+ * https://github.com/maxogden/modestmarker
+ * 
+ * MIT LICENSE
+ *
+ * depends on: modestmaps.js, smokesignals.js, zepto.js (see example for details)
+ */
+
 function ModestMarker (map, options) {
   var me = this
-  if (!options) options = {}
+
+  // make this an event emitter
+  smokesignals.convert(me)
   
-  Object.keys(options).forEach(function (key) {
-    me[key] = options[key]
-  })
+  $.extend(me, options || {})
   
   me.map = map
   if (typeof me.latitude === "string") me.latitude = parseFloat(me.latitude)
   if (typeof me.longitude === "string") me.longitude = parseFloat(me.longitude)
   me.location = new MM.Location(me.latitude, me.longitude)
-  me.div = document.createElement('div')
-  me.div.className = 'marker'
-  me.div.style.position = "absolute"
-  me.img = document.createElement('img')
-  me.img.src = me.markerImage || 'marker-solid-24.png'
-  me.div.appendChild(me.img)
+  
+  me.container = $('<div class="marker"></div>')
+  me.container.css('position', 'absolute')
+  me.image = $('<img src="' + (me.markerImage || 'marker-solid-24.png') + '">')
+  me.container.append(me.image)
+  me.map.parent.appendChild(me.container[0])
+  
   me.map.addCallback('panned', function() { me.updatePosition.call(me) })
   me.map.addCallback('zoomed', function() { me.updatePosition.call(me) })
   me.map.addCallback('resized', function() { me.updatePosition.call(me) })
-  me.map.parent.appendChild(me.div)
+  
+  me.touch = ('ontouchstart' in window) ? true : false
+  if (me.touch) me.map.parent.ontouchstart = function(e) { me.emit('mapClick', e) }
+  else me.map.parent.onmousedown = function(e) { me.emit('mapClick', e) }
+  
   me.updatePosition()
 }
 
 ModestMarker.prototype.updatePosition = function() {
   var me = this
-  var point = me.map.locationPoint(me.location)  
-  me.div.style.left = point.x - me.img.width / 2 + 'px'
-  me.div.style.top = point.y - me.img.height + 'px'
-  if (me.popup && me.popup.style.display === "block") me.showPopup()
+  var popupShowing = (me.popup && me.popup.css('display') !== "none")
+  var point = me.map.locationPoint(me.location)
+  me.container.css('left', point.x - me.image.width() / 2 + 'px')
+  var top = point.y - me.image.height()
+  if (popupShowing) top = top - me.popupHeight
+  me.container.css('top', top + 'px')
+  if (popupShowing) me.showPopup()
 }
+
+// popups are hidden by default so this makes them visible very quickly to get their height
+ModestMarker.prototype.getPopupHeight = function() {
+  var me = this
+  var offsetLeft = me.popup.css('left')
+  var display = me.popup.css('display')
+  me.popup.css({'left': "-9999px", 'display': "block"})
+  var height = me.popup.offsetHeight
+  me.popup.css({'left': offsetLeft, 'display': display})
+  return height
+}
+
 
 ModestMarker.prototype.setPopup = function(html, options) {
   var me = this
   me.offsetY = options.offsetY || 0
-  me.popup = document.createElement('div')
-  me.popup.className = 'popup'
-  me.popup.style.display = "none"
-  me.popup.style.position = "absolute"
-  me.popup.innerHTML = html
-  me.map.parent.appendChild(me.popup)
+  var popupStyle = "display: none; overflow: auto; position: relative; z-index: 666;"
+  me.popup = $('<div class="marker-popup" style="' + popupStyle + '">' + html + '</div>')
+  me.container.append(me.popup)
+  me.popupHeight = me.getPopupHeight()
   
-  function togglePopup() {
-    if (me.popup.style.display === "none") me.showPopup.call(me)
+  function togglePopup(e) {
+    if ((me.popup && me.popup.css('display') !== "none")) me.showPopup.call(me)
     else me.hidePopup.call(me)
   }
   
-  function dismissPopup(e) { if (e.target !== me.img) me.hidePopup.call(me) }
+  function dismissPopup(e) { if (e.target !== me.image) me.hidePopup.call(me) }
   
-  var touch = ('ontouchstart' in window) ? true : false
-  if (touch) me.div.ontouchstart = togglePopup
-  if (touch) me.map.parent.ontouchstart = dismissPopup
+  if (me.touch) me.container.ontouchstart = togglePopup
+  me.on('mapClick', dismissPopup)
   
-  var mouseHover = (options.hover && !touch)
-  if (mouseHover) me.div.onmouseover = function() { me.showPopup.call(me) }
-  if (mouseHover) me.div.onmouseout = function() { me.hidePopup.call(me) }
+  var mouseHover = (options.hover && !me.touch)
+  if (mouseHover) me.container.onmouseover = function() { me.showPopup.call(me) }
+  if (mouseHover) me.container.onmouseout = function() { me.hidePopup.call(me) }
   
-  var mouseClick = (!options.hover && !touch)
-  if (mouseClick) me.map.parent.onmousedown = dismissPopup
-  if (mouseClick) me.div.onmousedown = togglePopup
+  var mouseClick = (!options.hover && !me.touch)
+  if (mouseClick) me.container.onmousedown = togglePopup
 }
 
 ModestMarker.prototype.hidePopup = function(e) {
   var me = this
-  me.img.style.cursor = 'default'
-  me.popup.style.display = 'none'
+  me.image.css('cursor', 'default')
+  me.popup.hide()
 }
 
 ModestMarker.prototype.showPopup = function(e) {
   var me = this
-  me.popup.style.display = 'block'
-  me.popup.style.left = me.div.offsetLeft - .5 * me.popup.offsetWidth + .5 * me.img.width + 'px'
-  me.popup.style.top = me.div.offsetTop - me.popup.offsetHeight - me.offsetY + 'px'
-  me.img.style.cursor = 'pointer'
+  me.popup.css({
+    'display': 'block',
+    'left': "-" + me.image.width() / 2 + 'px',
+    'top': me.offsetY + 'px'
+  })
+  me.image.css('cursor', 'pointer')
 }
